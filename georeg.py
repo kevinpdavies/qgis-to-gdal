65472a4fda10372686accbb943fb0d2836667128
Kevin Davies <kevinpdavies@me.com>
2015-03-18 11:22:50 +1100

    Added shapefile mask to white out image regions

--- georeg.py
+++ georeg.py
@@ -7,10 +7,14 @@ import subprocess
 from qgisgcp2gdal import parse
 
 ''' Georegister images in ../pdf, using ../gcp, and output to ../tif'''
+''' Optional extent for output specified in ../ext '''
+''' Optional polygon shapefile of areas to "white out" in ../shp '''
 if __name__ == "__main__":
 
     # Get a list of PDF files based on the working folder
     pdfs = glob.glob(os.getcwd() + "/../pdf/*.pdf")
+    pdfs.sort()
+
     for pdf in pdfs:
 
         gcp = os.getcwd() + "/../gcp/" + basename(pdf) + ".points"
@@ -19,13 +23,31 @@ if __name__ == "__main__":
             continue
 
         with open(gcp) as f:
-            gdal_gcp = parse(f)
-            tmp_tif =  "/tmp/" + basename(pdf) + ".tif"
-            cmd = "/usr/bin/gdal_translate " + gdal_gcp + " -of GTiff -a_srs EPSG:4283 "
-            cmd += pdf + " " + tmp_tif
+
+            gcp = parse(f)
+            tmp =  "/tmp/" + basename(pdf) + ".tif"
+            cmd = "/usr/bin/gdal_translate " + gcp
+            cmd += " -of GTiff -a_srs EPSG:4283 "
+            cmd += pdf + " " + tmp
             subprocess.check_call(cmd, shell=True)
-            out_tif = os.getcwd() + "/../tif/" + basename(pdf).replace(".pdf", ".tif")
+
+            tif = os.getcwd() + "/../tif/" + \
+                basename(pdf).replace(".pdf", ".tif")
             cmd = "/usr/bin/gdalwarp -r near -order 1 -co COMPRESS=NONE "
-            cmd += tmp_tif + " " + out_tif
+            cmd += "-overwrite "
+            ext = os.getcwd() + "/../ext/" + basename(pdf) + ".ext"
+            if os.path.exists(ext):
+	    	with open(ext) as ext_f:
+                    cmd += "-te " + ext_f.read().rstrip("\n")
+            cmd += " " + tmp + " " + tif
             subprocess.check_call(cmd, shell=True)
-            os.remove(tmp_tif)
+            os.remove(tmp)
+
+            shp = os.getcwd() + "/../shp/" + \
+                basename(pdf).replace(".pdf", ".shp")
+            if os.path.exists(shp):
+                cmd = "gdal_rasterize -b 1 -b 2 -b 3 "
+                cmd += "-burn 255 -burn 255 -burn 255 "
+                cmd += "-l " + basename(shp)[:-4] + " " + shp + " "
+                cmd += tif
+                subprocess.check_call(cmd, shell=True)
